0835a3a46fbaada30d9c493017e68f410e5bbe5c
Nate Jaeger <Nate.RJS21@gmail.com>
2019-03-27 21:38:52 -0500

    Finished appendPageLinks, updated comments

--- js/script.js
+++ js/script.js
@@ -6,16 +6,6 @@ FSJS project 2 - List Filter and Pagination
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 const studentList = document.querySelector('.student-list').children;
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
 
 //Hide all students except the ones on page you want to show
 const showPage = (list, page) => {
@@ -36,14 +26,12 @@ const showPage = (list, page) => {
 showPage(studentList, 1);
 
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
+/* Function to take studentList and break it into correct amount of pages,
+   Then create and append the appropriate amount of li's and a tags,
+   Finally add event listeners to each a tag*/
 const appendPageLinks = (list) => {
-   //get the first instance of the page class which is the main container div
    const webPage = document.querySelector('.page');
+
    //Determine number of pages needed for given list
    const pages = Math.ceil(list.length / 10);
    
@@ -69,15 +57,17 @@ const appendPageLinks = (list) => {
    const aTags = document.querySelectorAll('a');
    
    //Loop over all a tags and add an event listener to each
-   for(let i = 1; i <= aTags.length; i++){
+   for(let i = 0; i < aTags.length; i++){
       //Give each a tag an event listener
       aTags[i].addEventListener('click', (e) => {
          showPage(list, e.target.textContent);
 
-         for(let i = 1; i <= pages; i++){
-            event.target.className = '';
+         //Clear active class from each aTag
+         for(let i = 0; i < pages; i++){
+            aTags[i].className = 'inActive';
          }
 
+         //assign the class active to the currently clicked button
          event.target.className = 'active';
       });
    }
